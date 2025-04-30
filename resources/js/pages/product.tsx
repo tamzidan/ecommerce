a85b0539d1c8b020e29c938275fcf619105c3948
tamzidan <tamzidan01@gmail.com>
2025-04-30 08:13:45 +0700

    add product page

---
+++ resources/js/pages/product.tsx
@@ -0,0 +1,10 @@
+import { Head, Link } from '@inertiajs/react';
+
+export default function Product() {
+
+    return (
+        <>
+            <p>Helloworld</p>
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\Log;
+
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -11,6 +13,11 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+
+Route::get('/product', function(){
+    Log::info("test");
+    return Inertia::render('product');
 });
 
 require __DIR__.'/settings.php';
